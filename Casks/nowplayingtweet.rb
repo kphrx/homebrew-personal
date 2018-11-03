3fc0be225bf6cdbc136fcc34a3457a1687bf465e
kPherox <admin@mail.kr-kp.com>
2018-11-04 02:35:13 +0900

    Add NowPlayingTweet

---
+++ Casks/nowplayingtweet.rb
@@ -0,0 +1,11 @@
+cask 'nowplayingtweet' do
+  version '0.5'
+  sha256 'b2b81f450ff62eb4bbbd4d321a9dde020ebb21e32fbc5d23c72b258de7b36dc5'
+
+  url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
+  appcast 'https://github.com/kPherox/NowPlayingTweet/releases.atom'
+  name 'NowPlayingTweet'
+  homepage 'https://github.kr-kp.com/NowPlayingTweet'
+
+  app 'NowPlayingTweet.app'
+end
