83d0b3418c3c84e9993b441ef0c553af0b06a409
kPherox <admin@mail.kr-kp.com>
2018-11-04 03:32:25 +0900

    Add comment of "When URL and Homepage Hostnames Differ"

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -2,6 +2,7 @@ cask 'nowplayingtweet' do
   version '0.5'
   sha256 'b2b81f450ff62eb4bbbd4d321a9dde020ebb21e32fbc5d23c72b258de7b36dc5'
 
+  # github.com/kPherox/NowPlayingTweet was verified as official when first introduced to the cask
   url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
   appcast 'https://github.com/kPherox/NowPlayingTweet/releases.atom'
   name 'NowPlayingTweet'
