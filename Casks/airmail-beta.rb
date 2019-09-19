259efd5829aa1d55b8bfae547653c310fa944c61
kPherox <admin@mail.kr-kp.com>
2019-09-19 13:19:30 +0900

    Change airmail-beta version to 3.7.0,393

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,5 +1,5 @@
 cask 'airmail-beta' do
-  version '3.7.0.704,549'
+  version '3.7.0,393'
   sha256 '47d4ecae81f27c1fca58bd96c196a91f12ef262f2c7b47ad19750e6cd37ee26d'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
