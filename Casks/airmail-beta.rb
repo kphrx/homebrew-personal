d858d4644e5c3f4976bf6076d5d484326b8d827d
kPherox <admin@mail.kr-kp.com>
2019-09-19 13:21:33 +0900

    Fix homepage url

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -2,11 +2,10 @@ cask 'airmail-beta' do
   version '3.7.0,393'
   sha256 '47d4ecae81f27c1fca58bd96c196a91f12ef262f2c7b47ad19750e6cd37ee26d'
 
-  # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04'
   name 'Airmail'
-  homepage 'https://airmailapp.com/beta/'
+  homepage 'https://rink.hockeyapp.net/apps/84be85c3331ee1d222fd7f0b59e41b04'
 
   app 'Airmail Beta.app'
 
