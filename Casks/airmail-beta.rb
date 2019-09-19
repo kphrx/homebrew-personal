8f166e5b7a3ef51e7595d5a9435b283e4460dad2
kPherox <admin@mail.kr-kp.com>
2019-09-19 12:08:04 +0900

    Add patch version

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,8 +1,8 @@
 cask 'airmail-beta' do
-  version '3.7.0,393'
+  version '3.7.0,549,393'
   sha256 '47d4ecae81f27c1fca58bd96c196a91f12ef262f2c7b47ad19750e6cd37ee26d'
 
-  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
+  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04'
   name 'Airmail'
   homepage 'https://rink.hockeyapp.net/apps/84be85c3331ee1d222fd7f0b59e41b04'
