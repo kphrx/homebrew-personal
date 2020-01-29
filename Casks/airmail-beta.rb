b80f98bf76d8b4e10a303c1e0a788d5007b215fd
kPherox <admin@mail.kr-kp.com>
2020-01-29 22:20:46 +0900

    Include mctoken to version

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,10 +1,9 @@
 cask 'airmail-beta' do
-  version '4.0,584,450'
-  mctoken '319416917c51df46dc3735596313a880fa441c26'
+  version '4.0,584,450:319416917c51df46dc3735596313a880fa441c26'
   sha256 '09f6728d73531b98150f888f913dffed5d31dbc01ec93bb9d9ab29ea96d5457f'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
-  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma.after_comma}?format=zip&mctoken=#{mctoken}"
+  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma.after_comma.before_colon}?format=zip&mctoken=#{version.after_comma.after_comma.after_colon}"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04'
   name 'Airmail'
   homepage 'https://airmailapp.com/beta/'
