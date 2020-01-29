d150eb4baf9732ba33affa00830064b4c2e03f70
kPherox <admin@mail.kr-kp.com>
2020-01-29 21:11:21 +0900

    Update airmail-beta to 4.0,450
    Homebrew/homebrew-cask-versions has been reintroduced. Follow this.

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,11 +1,12 @@
 cask 'airmail-beta' do
-  version '3.7.0,552,394'
-  sha256 '71bc6f43a5ffd8ec24276550b4e7239cce2c97ea6cc57ef47f9fd5e6086a65d2'
+  version '4.0,450'
+  sha256 '09f6728d73531b98150f888f913dffed5d31dbc01ec93bb9d9ab29ea96d5457f'
 
-  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma.after_comma}?format=zip&"
+  # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
+  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04'
   name 'Airmail'
-  homepage 'https://rink.hockeyapp.net/apps/84be85c3331ee1d222fd7f0b59e41b04'
+  homepage 'https://airmailapp.com/beta/'
 
   app 'Airmail Beta.app'
 
