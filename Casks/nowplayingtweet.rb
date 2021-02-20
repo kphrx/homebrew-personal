190f7736dca5374e92cb88ef37feef3d7f97caaa
kPherox <admin@mail.kr-kp.com>
2021-02-21 02:11:50 +0900

    nowplayingtweet: switch to `verified` parameter

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -2,8 +2,8 @@ cask "nowplayingtweet" do
   version "0.5.1"
   sha256 "d796b563d9461abffded73232fa69c4ba12f41867dd4ff058f631fc1f45d0572"
 
-  # github.com/kPherox/NowPlayingTweet was verified as official when first introduced to the cask
-  url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
+  url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip",
+      verified: "github.com/kPherox/NowPlayingTweet"
   appcast "https://github.com/kPherox/NowPlayingTweet/releases.atom"
   name "NowPlayingTweet"
   homepage "https://github.kr-kp.com/NowPlayingTweet"
