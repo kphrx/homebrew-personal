5b6cd387b9e516044aff100ab86447e74936c124
kPherox <admin@mail.kr-kp.com>
2021-02-21 02:06:48 +0900

    nowplayingtweet: fix RuboCop style

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -1,12 +1,12 @@
-cask 'nowplayingtweet' do
-  version '0.5.1'
-  sha256 'd796b563d9461abffded73232fa69c4ba12f41867dd4ff058f631fc1f45d0572'
+cask "nowplayingtweet" do
+  version "0.5.1"
+  sha256 "d796b563d9461abffded73232fa69c4ba12f41867dd4ff058f631fc1f45d0572"
 
   # github.com/kPherox/NowPlayingTweet was verified as official when first introduced to the cask
   url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
-  appcast 'https://github.com/kPherox/NowPlayingTweet/releases.atom'
-  name 'NowPlayingTweet'
-  homepage 'https://github.kr-kp.com/NowPlayingTweet'
+  appcast "https://github.com/kPherox/NowPlayingTweet/releases.atom"
+  name "NowPlayingTweet"
+  homepage "https://github.kr-kp.com/NowPlayingTweet"
 
-  app 'NowPlayingTweet.app'
+  app "NowPlayingTweet.app"
 end
