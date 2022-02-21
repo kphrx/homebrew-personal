68e56261fea660d2a568267194c5855425e8a9fb
kPherox <admin@mail.kr-kp.com>
2022-02-21 21:41:54 +0900

    add desc

--- Casks/font-cica-without-emoji.rb
+++ Casks/font-cica-without-emoji.rb
@@ -3,8 +3,8 @@ cask "font-cica-without-emoji" do
   sha256 "0574e5ebcbb33d445f7bea55fec46b9f67bd9b606b840b2d33b13f1e3c7aeef6"
 
   url "https://github.com/miiton/Cica/releases/download/v#{version}/Cica_v#{version}_without_emoji.zip"
-  appcast "https://github.com/miiton/Cica/releases.atom"
   name "Cica"
+  desc "プログラミング用日本語等幅フォント Cica(シカ)"
   homepage "https://github.com/miiton/Cica"
 
   conflicts_with cask: "homebrew/cask-fonts/font-cica"
