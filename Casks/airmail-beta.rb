7be22d4a47199e9a11c50e9bdd0051ed29f29bf1
kPherox <admin@mail.kr-kp.com>
2022-04-19 07:32:09 +0900

    Update airmail-beta from 6.0+679 to 6.1.3+694 (#29)

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,17 +1,12 @@
 cask "airmail-beta" do
   # version :latest
-  version "6.0+679,526"
+  version "6.1.3+694,536"
   # sha256 :no_check
-  sha256 "07992139ee8d3796f7493645504981afec36c36100805ff5c40f86970b05731e"
+  sha256 "efe648025d9b67c596f755829b661c571b6cdd4f695d8effa5d18e35755c8e73"
 
-  url do
-    require "open-uri"
+  url "https://install.appcenter.ms/api/v0.1/apps/airmail-devs-organization/airmail-beta/distribution_groups/all-users-of-airmail-beta/releases/#{version.after_comma}" do |page|
     require "json"
-    base_url = "https://install.appcenter.ms/api/v0.1/apps/airmail-devs-organization/airmail-beta/distribution_groups/all-users-of-airmail-beta/releases/#{version.after_comma}"
-    URI(base_url).open do |release_page|
-      latest_json = JSON.parse(release_page.read)
-      [latest_json["download_url"], { verified: "appcenter-filemanagement-distrib4ede6f06e.azureedge.net" }]
-    end
+    [JSON.parse(page)["download_url"], { verified: "azureedge.net" }]
   end
   name "Airmail Beta"
   desc "Lightning Fast Mail Client"
