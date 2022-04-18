8f26c2252a7d8f244af05d16b6e71cf73eddd9bf
kPherox <admin@mail.kr-kp.com>
2022-04-19 07:18:08 +0900

    Update font-cica-without-emoji from 5.0.2 to 5.0.3 (#30)

--- Casks/font-cica-without-emoji.rb
+++ Casks/font-cica-without-emoji.rb
@@ -1,6 +1,6 @@
 cask "font-cica-without-emoji" do
-  version "5.0.2"
-  sha256 "0574e5ebcbb33d445f7bea55fec46b9f67bd9b606b840b2d33b13f1e3c7aeef6"
+  version "5.0.3"
+  sha256 "e14c95c8c3e98d3778632f577a2cafa69ee82cd8c2efdb1f38c821e37c487dbf"
 
   url "https://github.com/miiton/Cica/releases/download/v#{version}/Cica_v#{version}_without_emoji.zip"
   name "Cica"
