70b1faadec5d97166b5d687f6375ed5c8686b41d
kPherox <admin@mail.kr-kp.com>
2023-01-18 04:18:35 +0900

    Update elk from 0.1.0 to 0.1.1

--- Casks/elk.rb
+++ Casks/elk.rb
@@ -1,9 +1,9 @@
 cask "elk" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.0"
-  sha256 arm:   "8f189ae1ca373b491240cfd95c5d7faf10a0058dcbfda15d6694a32186fd01b7",
-         intel: "c4f4901241776fa276b4420435db606635c13124f152e9a612bd42f5e9bafe80"
+  version "0.1.1"
+  sha256 arm:   "eb72fe051d57f382f4077a44f45a5a8b76ca9de963efb0e3caa8a99f8b1fbd59",
+         intel: "531de55c25395fcc9382f65cca99484166d74b184bef14559d7849c9459f7125"
 
   url "https://github.com/elk-zone/elk-native/releases/download/v#{version}/Elk_#{version}_macos_#{arch}.dmg"
   name "Elk Native"
