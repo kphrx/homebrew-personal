ed31626f15812c964d23ca93be0f03d3d7f53b05
kPherox <admin@mail.kr-kp.com>
2023-01-20 00:25:19 +0900

    Fix tag name for Elk Native

--- Casks/elk.rb
+++ Casks/elk.rb
@@ -5,13 +5,14 @@ cask "elk" do
   sha256 arm:   "eb72fe051d57f382f4077a44f45a5a8b76ca9de963efb0e3caa8a99f8b1fbd59",
          intel: "531de55c25395fcc9382f65cca99484166d74b184bef14559d7849c9459f7125"
 
-  url "https://github.com/elk-zone/elk-native/releases/download/v#{version}/Elk_#{version}_macos_#{arch}.dmg"
+  url "https://github.com/elk-zone/elk-native/releases/download/elk-native-v#{version}/Elk_#{version}_macos_#{arch}.dmg"
   name "Elk Native"
   desc "Native version of Elk, a nimble Mastodon web client"
   homepage "https://github.com/elk-zone/elk-native"
 
   livecheck do
     url :url
+    regex %r{href=.*?/tag/elk-native-v?(\d+(?:\.\d+)+)["' >]}i
     strategy :github_latest
   end
 
