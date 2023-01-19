86184baea2e7520223a8796b964e40b0e0d1fdb5
kPherox <admin@mail.kr-kp.com>
2023-01-20 01:00:01 +0900

    Update Elk Native from 0.1.1 to 0.2.0 (#35)

--- Casks/elk.rb
+++ Casks/elk.rb
@@ -1,17 +1,18 @@
 cask "elk" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.1"
-  sha256 arm:   "eb72fe051d57f382f4077a44f45a5a8b76ca9de963efb0e3caa8a99f8b1fbd59",
-         intel: "531de55c25395fcc9382f65cca99484166d74b184bef14559d7849c9459f7125"
+  version "0.2.0"
+  sha256 arm:   "0d3124dfc666157e99206c28c569ceb47d48ca11f4465fe3edda8cc25a5143ca",
+         intel: "6e109fc83f7754250c9968d72a14420adaef9f2e8b46e18aa54ddd5f44534d9e"
 
-  url "https://github.com/elk-zone/elk-native/releases/download/v#{version}/Elk_#{version}_macos_#{arch}.dmg"
+  url "https://github.com/elk-zone/elk-native/releases/download/elk-native-v#{version}/Elk_#{version}_macos_#{arch}.dmg"
   name "Elk Native"
   desc "Native version of Elk, a nimble Mastodon web client"
   homepage "https://github.com/elk-zone/elk-native"
 
   livecheck do
     url :url
+    regex %r{href=.*?/tag/elk-native-v?(\d+(?:\.\d+)+)["' >]}i
     strategy :github_latest
   end
 
