020f0925d9d1d36decccb2e98309aefb4c505feb
kPherox <kphrx@kpherox.dev>
2023-06-15 06:00:00 +0900

    font-line-seed-jp latest

--- Casks/font-line-seed-jp.rb
+++ Casks/font-line-seed-jp.rb
@@ -1,5 +1,5 @@
 cask "font-line-seed-jp" do
-  version "20221014"
+  version :latest
   sha256 :no_check
 
   url "https://seed.line.me/src/images/fonts/LINE_Seed_Sans_JP.zip"
@@ -8,38 +8,38 @@ cask "font-line-seed-jp" do
   homepage "https://seed.line.me/index_jp.html"
 
   # TTF for App
-  font "LINE_Seed_JP_#{version}/App/TTF/LINESeedJP_A_TTF_Rg.ttf"
-  font "LINE_Seed_JP_#{version}/App/TTF/LINESeedJP_A_TTF_Eb.ttf"
-  font "LINE_Seed_JP_#{version}/App/TTF/LINESeedJP_A_TTF_Th.ttf"
-  font "LINE_Seed_JP_#{version}/App/TTF/LINESeedJP_A_TTF_Bd.ttf"
+  font "LINE_Seed_JP/App/TTF/LINESeedJP_A_TTF_Rg.ttf"
+  font "LINE_Seed_JP/App/TTF/LINESeedJP_A_TTF_Eb.ttf"
+  font "LINE_Seed_JP/App/TTF/LINESeedJP_A_TTF_Th.ttf"
+  font "LINE_Seed_JP/App/TTF/LINESeedJP_A_TTF_Bd.ttf"
   # OTF for App
-  font "LINE_Seed_JP_#{version}/App/OTF/LINESeedJP_A_OTF_Eb.otf"
-  font "LINE_Seed_JP_#{version}/App/OTF/LINESeedJP_A_OTF_Bd.otf"
-  font "LINE_Seed_JP_#{version}/App/OTF/LINESeedJP_A_OTF_Th.otf"
-  font "LINE_Seed_JP_#{version}/App/OTF/LINESeedJP_A_OTF_Rg.otf"
+  font "LINE_Seed_JP/App/OTF/LINESeedJP_A_OTF_Eb.otf"
+  font "LINE_Seed_JP/App/OTF/LINESeedJP_A_OTF_Bd.otf"
+  font "LINE_Seed_JP/App/OTF/LINESeedJP_A_OTF_Th.otf"
+  font "LINE_Seed_JP/App/OTF/LINESeedJP_A_OTF_Rg.otf"
   # WOFF for Web
-  font "LINE_Seed_JP_#{version}/Web/WOFF/LINESeedJP_OTF_Bd.woff"
-  font "LINE_Seed_JP_#{version}/Web/WOFF/LINESeedJP_OTF_Eb.woff"
-  font "LINE_Seed_JP_#{version}/Web/WOFF/LINESeedJP_OTF_Rg.woff"
-  font "LINE_Seed_JP_#{version}/Web/WOFF/LINESeedJP_OTF_Th.woff"
+  font "LINE_Seed_JP/Web/WOFF/LINESeedJP_OTF_Bd.woff"
+  font "LINE_Seed_JP/Web/WOFF/LINESeedJP_OTF_Eb.woff"
+  font "LINE_Seed_JP/Web/WOFF/LINESeedJP_OTF_Rg.woff"
+  font "LINE_Seed_JP/Web/WOFF/LINESeedJP_OTF_Th.woff"
   # WOFF2 for Web
-  font "LINE_Seed_JP_#{version}/Web/WOFF2/LINESeedJP_OTF_Rg.woff2"
-  font "LINE_Seed_JP_#{version}/Web/WOFF2/LINESeedJP_OTF_Bd.woff2"
-  font "LINE_Seed_JP_#{version}/Web/WOFF2/LINESeedJP_OTF_Eb.woff2"
-  font "LINE_Seed_JP_#{version}/Web/WOFF2/LINESeedJP_OTF_Th.woff2"
+  font "LINE_Seed_JP/Web/WOFF2/LINESeedJP_OTF_Rg.woff2"
+  font "LINE_Seed_JP/Web/WOFF2/LINESeedJP_OTF_Bd.woff2"
+  font "LINE_Seed_JP/Web/WOFF2/LINESeedJP_OTF_Eb.woff2"
+  font "LINE_Seed_JP/Web/WOFF2/LINESeedJP_OTF_Th.woff2"
   # EOT for Web
-  font "LINE_Seed_JP_#{version}/Web/EOT/LINESeedJP_TTF_Eb.eot"
-  font "LINE_Seed_JP_#{version}/Web/EOT/LINESeedJP_TTF_Bd.eot"
-  font "LINE_Seed_JP_#{version}/Web/EOT/LINESeedJP_TTF_Th.eot"
-  font "LINE_Seed_JP_#{version}/Web/EOT/LINESeedJP_TTF_Rg.eot"
+  font "LINE_Seed_JP/Web/EOT/LINESeedJP_TTF_Eb.eot"
+  font "LINE_Seed_JP/Web/EOT/LINESeedJP_TTF_Bd.eot"
+  font "LINE_Seed_JP/Web/EOT/LINESeedJP_TTF_Th.eot"
+  font "LINE_Seed_JP/Web/EOT/LINESeedJP_TTF_Rg.eot"
   # TTF for Desktop
-  font "LINE_Seed_JP_#{version}/Desktop/TTF/LINESeedJP_TTF_Th.ttf"
-  font "LINE_Seed_JP_#{version}/Desktop/TTF/LINESeedJP_TTF_Bd.ttf"
-  font "LINE_Seed_JP_#{version}/Desktop/TTF/LINESeedJP_TTF_Eb.ttf"
-  font "LINE_Seed_JP_#{version}/Desktop/TTF/LINESeedJP_TTF_Rg.ttf"
+  font "LINE_Seed_JP/Desktop/TTF/LINESeedJP_TTF_Th.ttf"
+  font "LINE_Seed_JP/Desktop/TTF/LINESeedJP_TTF_Bd.ttf"
+  font "LINE_Seed_JP/Desktop/TTF/LINESeedJP_TTF_Eb.ttf"
+  font "LINE_Seed_JP/Desktop/TTF/LINESeedJP_TTF_Rg.ttf"
   # OTF for Desktop
-  font "LINE_Seed_JP_#{version}/Desktop/OTF/LINESeedJP_OTF_Rg.otf"
-  font "LINE_Seed_JP_#{version}/Desktop/OTF/LINESeedJP_OTF_Bd.otf"
-  font "LINE_Seed_JP_#{version}/Desktop/OTF/LINESeedJP_OTF_Th.otf"
-  font "LINE_Seed_JP_#{version}/Desktop/OTF/LINESeedJP_OTF_Eb.otf"
+  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Rg.otf"
+  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Bd.otf"
+  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Th.otf"
+  font "LINE_Seed_JP/Desktop/OTF/LINESeedJP_OTF_Eb.otf"
 end
