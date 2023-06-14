0b8e19a23c9abaaef8b847537a63b05a4eb4a116
kPherox <admin@mail.kr-kp.com>
2023-06-15 04:52:57 +0900

    Update nowplayingtweet link (#38)
    * update nowplayingtweet livecheck strategy

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -2,15 +2,28 @@ cask "nowplayingtweet" do
   version "0.5.1"
   sha256 "d796b563d9461abffded73232fa69c4ba12f41867dd4ff058f631fc1f45d0572"
 
-  url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip",
-      verified: "github.com/kPherox/NowPlayingTweet"
+  url "https://github.com/nowplayingtweet/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
   name "NowPlayingTweet"
   desc "Tweet #NowPlaying with iTunes"
-  homepage "https://github.kpherox.dev/NowPlayingTweet"
+  homepage "https://github.com/nowplayingtweet/NowPlayingTweet"
 
   livecheck do
     url :url
-    strategy :github_latest
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        value = T.let(nil, T.untyped)
+        version_keys = ["tag_name", "name"]
+        version_keys.find do |key|
+          match = release[key]&.match(regex)
+          next if match.blank?
+
+          value = match[1]
+        end
+        value
+      end
+    end
   end
 
   app "NowPlayingTweet.app"
