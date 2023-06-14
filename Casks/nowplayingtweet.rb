3977604ac5ff30a76d4805d8e2d648b1cdb1f620
kPherox <admin@mail.kr-kp.com>
2023-06-15 03:52:10 +0900

    Update nowplayingtweet.rb

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -2,11 +2,10 @@ cask "nowplayingtweet" do
   version "0.5.1"
   sha256 "d796b563d9461abffded73232fa69c4ba12f41867dd4ff058f631fc1f45d0572"
 
-  url "https://github.com/kPherox/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip",
-      verified: "github.com/kPherox/NowPlayingTweet"
+  url "https://github.com/nowplayingtweet/NowPlayingTweet/releases/download/#{version}/NowPlayingTweet-#{version}.zip"
   name "NowPlayingTweet"
   desc "Tweet #NowPlaying with iTunes"
-  homepage "https://github.kpherox.dev/NowPlayingTweet"
+  homepage "https://github.com/nowplayingtweet/NowPlayingTweet"
 
   livecheck do
     url :url
