1868878887d727f2c58e8ada5b6323023ce8bb6f
kPherox <kphrx@kpherox.dev>
2026-08-03 23:26:18 +0900

    not define macos version for nowplayingtweet

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -28,12 +28,13 @@ cask "nowplayingtweet" do
 
   deprecate! date: "2023-02-02", because: :discontinued
 
-  depends_on macos: [
-    # :mojave,
-    # :high_sierra,
-    # :sierra,
-    # :el_capitan,
-  ]
+  depends_on :macos
+  # depends_on macos: [
+  #   :mojave,
+  #   :high_sierra,
+  #   :sierra,
+  #   :el_capitan,
+  # ]
 
   app "NowPlayingTweet.app"
 end
