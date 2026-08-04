b6e6edefbb9f84ba5dbc1843ca9e2a66d9d21d9e
kPherox <kphrx@kpherox.dev>
2026-08-04 12:28:39 +0900

    fix Cask/StanzaGrouping

--- Casks/nowplayingtweet.rb
+++ Casks/nowplayingtweet.rb
@@ -30,7 +30,8 @@ cask "nowplayingtweet" do
 
   depends_on arch: :intel
   depends_on :macos
-  # depends_on macos: [
+
+    # depends_on macos: [
   #   :mojave,
   #   :high_sierra,
   #   :sierra,
